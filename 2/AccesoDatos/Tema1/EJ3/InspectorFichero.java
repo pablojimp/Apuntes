425f2ffac189fe4e022504d93325937e910025d9
pablojimp <pablo.jimp21@gmail.com>
2026-09-23 22:14:38 +0200

    Ejercicio 3 Terminado

---
+++ 2/AccesoDatos/Tema1/EJ3/InspectorFichero.java
@@ -0,0 +1,24 @@
+package EJ3;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class InspectorFichero {
+    public static void main(String[] args) {
+        Path ruta = Path.of("datos", "clubes.txt");
+        System.out.println("Ruta Absoluta : " + ruta.toAbsolutePath());
+
+        try {
+            if (!Files.exists(ruta)) {
+                System.out.println("Este Archivo todavia no existe");
+            } else {
+                System.out.println("Este Archivo existe y pesa: " + Files.size(ruta) + "bytes");
+
+            }
+
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+}
